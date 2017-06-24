e06dacdb570bcc9dd0890a3edba1e4f184323566
Hu Zhang <rabbit@Hus-MacBook-Air.local>
2017-06-25 01:29:42 +1000

    comments added

--- src/backend/OnceAPI.js
+++ src/backend/OnceAPI.js
@@ -2,12 +2,26 @@ import express from "express"
 import bodyParser from "body-parser";
 
 const getUri = (view, method) => {
+    /**
+     * Generate url based on View class's name and method name
+     * e.g.
+     * class MyView extends View {
+     *     helloWorld() {
+     *     }
+     * }
+     *
+     * MyView.helloWorld would produce my-view/hello-world
+     */
     return `/${slugify(view.name)}/${slugify(method)}`;
 };
 
 const LETTER_REGEX = /([A-Z])/g;
 
 const slugify = (str) => {
+    /**
+     *  transform camelCase (or PascalCase) to hyphen-connected case
+     *  e.g.: HelloWorld -> hello-world
+     */
     let count = 0;
     return str.replace(LETTER_REGEX, letter => {
         if (count++ === 0) {
@@ -18,23 +32,34 @@ const slugify = (str) => {
     });
 };
 
-const getViewClassMethods = viewClass => Object.getOwnPropertyNames(viewClass.prototype).filter(name => typeof viewClass.prototype[name] === "function" && name !== "constructor");
+const getViewClassMethods = viewClass => {
+    /**
+     * return a list of methods of the view class,
+     * excluding the constructor
+     */
+    return Object.getOwnPropertyNames(viewClass.prototype).filter(
+        name => typeof viewClass.prototype[name] === "function" && name !== "constructor"
+    );
+};
 
 export default class OnceAPI {
     constructor(views, prefix = "", mappingsURI = "/__mappings__") {
         let app = express();
-        app.use(bodyParser.json());
+        app.use(bodyParser.json()); //body-parser for parsing arguments
         let uriRegistered = {};
-        let mappings = {};
+        let mappings = {}; //used to store class/method to uri
         views.forEach(view => {
             mappings[view.name] = {};
             getViewClassMethods(view).forEach(method => {
                 let uri = getUri(view, method);
                 if (uriRegistered[uri]) {
+                    // method name conflicts: e.g. greet and Greet would generate uri "greet"
                     throw `URI conflict detected for ${view.name}.${method} and ${uriRegistered[uri]}: ${uri}`;
                 }
+                // record class/method for error throwing if conflict dectected
                 uriRegistered[uri] = `${view.name}.${method}`;
                 mappings[view.name][method] = `${prefix}${uri}`;
+                // register the urls
                 app.post(uri, (req, res, next) => {
                     let v = new view(req, res, next);
                     v[method].apply(v, req.body);
@@ -47,7 +72,7 @@ export default class OnceAPI {
             enumerable: false,
             configurable: false
         };
-
+        // All these three values are read-only
         Object.defineProperties(this, {
             app: Object.assign({
                 value: app
@@ -60,7 +85,8 @@ export default class OnceAPI {
             }, defaultProps)
         });
 
-        Object.seal(this);
+        Object.seal(this); // seal the instance so it can't be changed anymore
+        // register the uri for frontend to retrieve the "class/method->uri" mappings
         this.app.post(this.mappingsURI, (req, res) => res.json(this.mappings));
     }
 }
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -26,21 +26,33 @@ const setReadOnly = (obj, key, value) => {
 const OnceAPI = (mappingsURI = "/__mappings__") => {
     let OnceAPI = Object.create(null);
     setReadOnly(OnceAPI, "ready", new Promise((resolve, reject) => {
-        post(mappingsURI).then(classes => {
+        post(mappingsURI).then(classes => { // retrieve method->uri mappings
+            /** it looks like this:
+              * {
+              *     "MyView": { // "MyView" here is value of "className" in the following code
+              *         "myMethod": "/my-view/my-method"
+              *         // "myMethod" is "methodName", and "/my-view/my-method" is "uri"
+              *      }
+              * }
+              */
             let constructedClasses = Object.create(null);
             for (let className in classes) {
                 let cls = classes[className];
                 let methods = Object.create(null);
 
-                for (let method in cls) {
-                    let path = cls[method];
-                    setReadOnly(methods, method, function() {
-                        return post(path, Array.prototype.slice.call(arguments));
+                for (let methodName in cls) {
+                    let uri = cls[methodName];
+                    setReadOnly(methods, methodName, function() {
+                        // the method simply returns a promise of the xhr call
+                        // arguments are JSON stringified, so all arguments must be JSON serializable
+                        return post(uri, Array.prototype.slice.call(arguments));
                     });
                 }
                 
-                Object.seal(methods);
+                Object.seal(methods); // finalize the methods
                 setReadOnly(constructedClasses, className, methods);
+                // also attach the class to OnceAPI, so it can be invoked like this:
+                // OnceAPI.ready(() => OnceAPI.MyClass.myMethod("some arguments"))
                 setReadOnly(OnceAPI, className, methods);
             }
             Object.seal(OnceAPI);
